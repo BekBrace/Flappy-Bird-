0c2d2e4eff54fc9e13f96b29c815d337bc336560
BekBrace <bekdevelopment@gmail.com>
2022-11-01 13:42:38 +0100

    initial commmit

---
+++ game.js
@@ -0,0 +1,118 @@
+// Now the fun part begins 😀
+
+// Declaring constants
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const img = new Image();
+img.src = "https://i.ibb.co/GTB9dP0/flappy-bird-set.png";
+
+// General settings and more constant declarations
+let gamePlaying = false;
+const gravity = .5;
+const speed = 6.2;
+const size = [51, 36];
+const jump = -11.5;
+const cTenth = (canvas.width / 10);
+
+let index = 0,
+    bestScore = 0, 
+    flight, 
+    flyHeight, 
+    currentScore, 
+    pipe;
+
+// Pipe Settings
+const pipeWidth = 78;
+const pipeGap = 270;
+const pipeLoc = () => (Math.random() * ((canvas.height -(pipeGap + pipeWidth)) - pipeWidth)) + pipeWidth;
+const setup = () => {
+    currentScore = 0;
+    flight = jump;
+
+    // set initial fly height (middle of screen - size of the bird)
+    flyHeight = (canvas.height / 2) - (size[1] /2);
+    
+    // setup first 3 pipes
+    pipes = Array(3).fill().map((a,i) => [canvas.width + (i * (pipeGap + pipeWidth)), pipeLoc()]);
+}
+
+const render = () => {
+    // make the pipe and bird mobing
+    index++;
+
+    // background first part
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height, -((index * (speed /2)) % canvas.width) + canvas.width, 0, canvas.width,  canvas.height);
+
+    // background second part
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height, -(index * (speed / 2)) % canvas.width, 0, canvas.width, canvas.height);
+
+    // pipe display
+    if (gamePlaying) {
+        pipes.map (pipe => {
+            // pipe moving
+            pipe[0] -= speed;
+
+            // top pipe
+            ctx.drawImage(img, 432, 588 - pipe[1], pipeWidth, pipe[1], pipe[0], 0, pipeWidth, pipe[1]);
+
+            // bottom pipe
+            ctx.drawImage(img, 432 + pipeWidth, 108, pipeWidth, canvas.height - pipe[1] + pipeGap, pipe[0], pipe[1] + pipeGap, pipeWidth, canvas.height - pipe[1] + pipeGap);
+
+            // Give 1 point and create new pipe
+            if (pipe[0] <= -pipeWidth) {
+                currentScore ++;
+                // check if it's the best score
+                bestScore = Math.max(bestScore, currentScore);
+
+                // remove and create new pipe
+                pipes = [ ...pipes.slice(1), [pipes[pipes.length-1][0] + pipeGap + pipeWidth, pipeLoc()]];
+                // eventually you can console log the pipes variable
+                // console.log(pipes); 
+            }
+
+            // Collision detection
+            if ([
+                pipe[0] <= cTenth + size[0],
+                pipe[0] + pipeWidth >=  cTenth,
+                pipe[1] > flyHeight || pipe[1] + pipeGap < flyHeight + size[1]
+            ] .every(elem => elem)) {
+                gamePlaying = false;
+                // invoke the setup function
+                setup();
+            }
+        })
+    }    
+    // Draw bird
+    if (gamePlaying) {
+     
+        ctx.drawImage(img, 432, Math.floor((index % 9) / 3)* size[1], ...size, cTenth, flyHeight, ...size);
+     
+        flight += gravity;
+     
+        flyHeight = Math.min(flyHeight + flight, canvas.height - size[1]);
+    } else {
+     
+        ctx.drawImage(img, 432, Math.floor((index % 9) / 3) * size[1], ...size, ((canvas.width/2) - size[0] / 2), flyHeight, ...size);
+     
+        flyHeight = (canvas.height / 2) - (size[1] /2);
+
+        // text
+        ctx.fillText(`Best Score: ${bestScore}`, 85, 245);
+        ctx.fillText('Click to play', 90, 535);
+        ctx.font = "bold 30px courier";
+    }
+
+    document.getElementById('bestScore').innerHTML = `Best: ${bestScore}`;
+    document.getElementById('currentScore').innerHTML = `Current : ${currentScore}`;
+
+    // Tell the browser to perform animation
+    window.requestAnimationFrame(render)
+}
+
+// Launch setup
+setup();
+img.onload = render;
+
+// Start the game
+document.addEventListener('click', () => gamePlaying = true);
+window.onclick = () => flight = jump;
